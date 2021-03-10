6ffe90ae8013fec36156215fac0ffa11a0188806
Paayk <itsabdennacerama@gmail.com>
2021-03-10 18:43:38 +0100

    preparing for parsing

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# minishell-akatsuki
+
+# Nothing to see here.
---
+++ srcs/parsing/input_parser.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_parser.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdait-m <abdait-m@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/10 18:38:31 by abdait-m          #+#    #+#             */
+/*   Updated: 2021/03/10 18:42:18 by abdait-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct      o_parse
+{
+    char *cmds;
+    char **args;
+    char *redirs;
+}                   p_parse;
+
+int main()
+{
+    return 0;
+}

23d65c8313a891c87aae50b4a2cca6a14eaf34ea
Paayk <itsabdennacerama@gmail.com>
2021-03-16 16:33:20 +0100

    --

--- srcs/ft_strtok.c
+++ srcs/ft_strtok.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <../includes/minishell.h>
+#include "../includes/minishell.h"
 
 
 char        *my_strtok(char *s, char *check)
